524a3ad74f84ce6979438a9cfbb843feeab9bbaf
Adam <adam.lee.hensley@icloud.com>
2023-11-28 15:13:48 -0700

    adding bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {NgbInputDatepicker, NgbModal, NgbDropdownModule} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NgbInputDatepicker, NgbDropdownModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
+  constructor(private modalService: NgbModal) {
+  }
+  public open(modal: any): void {
+    this.modalService.open(modal);
+  }
+
   title = 'personal-ui';
 }
